73427bdc252e49f0cbbf928b4cc5f9215e05f65e
S.E.H.H <90520252+SEHHxrc@users.noreply.github.com>
2022-04-06 18:21:09 +0800

    Add files via upload

--- scanner/main.go
+++ scanner/main.go
@@ -4,34 +4,68 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ./scanner <ip>")
+	ip, max := parse()
+	if ip == "" {
 		return
 	}
+
 	var openPort []int
 	wg := sync.WaitGroup{}
-	for port := 0; port < 65536; port++ {
+	//scan port
+	fmt.Printf("Scanning %s from 0 to %d\n", ip, max)
+	for port := 0; port < max; port++ {
 		wg.Add(1)
 		go func(p int) {
-			if open(p) {
+			if open(ip, p) {
 				openPort = append(openPort, p)
 			}
 			defer wg.Done()
 		}(port)
 	}
 	wg.Wait()
+
 	for _, v := range openPort {
 		fmt.Printf("Port %d: success\n", v)
 	}
 }
 
-func open(port int) (flag bool) {
-	_, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", os.Args[1], port), time.Millisecond*200)
+func parse() (ip string, max int) {
+	// Parse parameters
+	if len(os.Args) != 2 && len(os.Args) != 4 {
+		fmt.Println("Usage: ./scanner <ip> [option]\n\t-m\tMaximum port number")
+		return
+	}
+
+	ip = os.Args[1]
+	max = 65536
+	var err error
+
+	if len(os.Args) == 4 {
+		if os.Args[2] == "-m" {
+			max, err = strconv.Atoi(os.Args[3])
+			if err != nil || max <= 0 {
+				fmt.Println("port number must be a positive number which is not greater than 65536")
+				ip = ""
+				return
+			}
+
+		} else {
+			fmt.Println("Usage: ./scanner <ip> [option]\n\t-m\tMaximum port number")
+			ip = ""
+			return
+		}
+	}
+	return
+}
+
+func open(ip string, port int) (flag bool) {
+	_, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, port), time.Millisecond*200)
 	if err == nil {
 		flag = true
 	}
